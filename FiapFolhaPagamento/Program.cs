900b2636f3a56c08cb321fd61e8914c496537b91
kenzotanaka17 <andre.kenzo176@gmail.com>
2024-09-04 20:18:12 -0300

    Criando faixas INSS

--- FiapFolhaPagamento/Program.cs
+++ FiapFolhaPagamento/Program.cs
@@ -18,4 +18,25 @@
 
         public decimal Aliquota { get; set; }
     }
+
+    public class Inss
+    {
+
+        public Inss()
+        {
+            Faixas = new List<INSSFaixa>
+            {
+             new INSSFaixa { Piso = 0, Teto = 1412, Aliquota = 7.5m },
+             new INSSFaixa { Piso = 1412.01m, Teto = 2666.68m, Aliquota = 9 },
+             new INSSFaixa { Piso = 2666.69m, Teto = 4000.03m, Aliquota = 12 },
+             new INSSFaixa { Piso = 4000.04m, Teto = 7786.02m, Aliquota = 14 }
+            };
+        }
+        public List<INSSFaixa>Faixas { get; set; }
+
+        public decimal CalcularDesconto(decimal salarioBruto)
+        {
+            return 0;
+        }
+    }
 }
